42818ec4a76b91aaf0a0668a254e10c01dba321e
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-29 23:27:17 +0600

    Connect frontend static files

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ mongoose.connect(process.env.MONGODB_URI, { })
   .catch((error) => console.error("MongoDB connection error:", error));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+//app.use('/api/appointments', appointmentRoutes);
 app.use('/appointments', appointmentRoutes);
+
+//Serve 'index.html' file at the root of the server
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
