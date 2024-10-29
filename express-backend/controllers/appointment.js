13c879fba5ae8f80d8996781965b46f68debd39c
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-29 23:18:17 +0600

    as data for this database are object transform req.params.id to an ObjectId  and note: mongoose is a class

--- express-backend/controllers/appointment.js
+++ express-backend/controllers/appointment.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Appointment = require('../models/appointment.js');
 
 exports.createAppointment = async (req, res) => {
@@ -21,16 +22,24 @@ exports.getAppointments = async (req, res) => {
 
 exports.updateAppointment = async (req, res) => {
   try {
-    const updatedAppointment = await Appointment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedAppointment = await Appointment.findByIdAndUpdate(
+      new mongoose.Types.ObjectId(req.params.id),
+      req.body,
+      { new: true }
+    );
+    if (!updatedAppointment) {
+      return res.status(404).json({ error: 'Appointment not found' });
+    }    
     res.json(updatedAppointment);
   } catch (error) {
+    console.error(error);
     res.status(400).json({ error });
   }
 };
 
 exports.deleteAppointment = async (req, res) => {
   try {
-    await Appointment.findByIdAndDelete(req.params.id);
+    await Appointment.findByIdAndDelete( new mongoose.Types.ObjectId(req.params.id));
     res.status(204).send();
   } catch (error) {
     res.status(500).json({ error });
