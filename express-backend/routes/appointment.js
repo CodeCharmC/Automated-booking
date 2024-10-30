157eb642f15f11a42c0c0521c2d4c06f5414562e
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-30 23:46:00 +0600

    Modify appointment.js routes

--- express-backend/routes/appointment.js
+++ express-backend/routes/appointment.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointment.js');
+const authMiddleware = require('../middlewares/auth.js');
 
+//for patients
 router.post('/', appointmentController.createAppointment);
-router.get('/', appointmentController.getAppointments);
-router.get('/:id', appointmentController.getSingleAppointments);
-router.patch('/:id', appointmentController.updateAppointment);
-router.delete('/:id', appointmentController.deleteAppointment);
+router.get('/:id/details', appointmentController.appointmentDetails);      //need to be fixed
+
+//for doctor
+router.get('/', authMiddleware, appointmentController.getAppointments);
+router.get('/:id', authMiddleware, appointmentController.getSingleAppointment);
+router.patch('/:id', authMiddleware, appointmentController.updateAppointment);
+router.delete('/:id', authMiddleware, appointmentController.deleteAppointment);
 
 module.exports = router;
