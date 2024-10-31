e696af3935ea30397147741341294ac75d4c8027
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-31 20:53:36 +0600

    fix bug for server shutting down

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Internal Server Error');
 })
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
