033cd34bb5daba7ea9d5f4ed0450065c00cb0ad3
CodeCharmC <moni.akhtar1066@gmail.com>
2024-11-11 19:21:47 +0600

    adjust auth for the avatar

--- express-backend/controllers/auth.js
+++ express-backend/controllers/auth.js
@@ -5,13 +5,17 @@ const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expires
 
 exports.register = async (req, res) => {
    try {
-      const { username, password, specialty, experience, officeHours } = req.body;
+      const { username, password, specialty, experience, officeHours, avatar } = req.body;
       
       if (!username || !password || !specialty || !experience || !officeHours) {
          return res.status(400).json({ error: 'All fields are required: username, password, specialty, experience, and office hours.' });
       }
-      
-      const doctor = new Doctor({ username, password, specialty, experience, officeHours });
+
+      if (!avatar) {
+         avatar = '/assets/profile.jpg'
+      }
+      console.log(avatar);
+      const doctor = new Doctor({ username, password, specialty, experience, officeHours, avatar });
       await doctor.save();
 
       res.status(201).json({ message: 'Doctor registered successfully' });
