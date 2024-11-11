2917ec403435e53124a795a0d70cab57991db0d1
CodeCharmC <moni.akhtar1066@gmail.com>
2024-11-11 20:36:51 +0600

    genarates doctors from database

--- frontend/doctors.js
+++ frontend/doctors.js
@@ -1,12 +1,26 @@
 async function loadDoctors() {
-   const response = await fetch('/doctor');
+   const response = await fetch('/doctor/');   
    const doctors = await response.json();
    const doctorsList = document.getElementById('doctors-list');
 
    doctors.forEach(doctor => {
-   const listItem = document.createElement('li');
-   listItem.textContent = `${doctor.name} - ${doctor.specialty}`;
-   doctorsList.appendChild(listItem);
+      const doctorHTml = `
+      <li>
+         <div class="doctor-card">
+            <div class="doctor-info">
+               <h3>${doctor.username}</h3>
+               <p>Specialty: ${doctor.specialty}</p>
+               <p>Experience: ${doctor.experience}years</p>
+               <p>Office Hours: ${doctor.officeHours}</p>
+               <button class="book-appointment"><a href="create-appointment.html?doctorId=${doctor._id}">Book Appointment</a></button>
+            </div>
+            <div>
+               <img src=${doctor.avatar} alt="${doctor.username}">
+            </div>
+         </div>
+      </li>
+      `;
+      doctorsList.innerHTML += doctorHTml;
    });
 }
 
